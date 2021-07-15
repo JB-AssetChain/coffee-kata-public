229d36c70af0f34ee5d97f8b8c8954cf295af4f1
Nick <nick@192-168-1-114.tpgi.com.au>
2021-07-15 16:31:38 +1000

    Init

---
+++ index.js
@@ -0,0 +1,4 @@
+function coffeeMachine(a, b) {
+    return a + b;
+}
+module.exports = coffeeMachine;
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+const coffeeMachine = require('../index');
+
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 +2).toBe(3);
+  expect(coffeeMachine(1, 2)).toBe(3);
 });
