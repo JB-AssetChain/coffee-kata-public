313b28eecb4a099dada58dcc145c641875201953
Tim Gaul <tim@getfu.com.au>
2021-07-15 17:10:24 +1000

    Test commit

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,26 @@
-const coffeeMachine = require('../index');
+const coffeeMachine = require("../index");
 
 test('Given "T:1:0" returns "Drink maker makes 1 tea with 1 sugar and a stick"', () => {
-    //Given
-    const input = "T:1:0";
-    const expected = "Drink maker makes 1 tea with 1 sugar and a stick";
+  //Given
+  const input = "T:1:0";
+  const expected = "Drink maker makes 1 tea with 1 sugar and a stick";
 
-    //When
-    const result = coffeeMachine(input);
+  //When
+  const result = coffeeMachine(input);
 
-    //Then
-    expect(result).toBe(expected);
+  //Then
+  expect(result).toBe(expected);
 });
+/// comment
 test('Given "H::" returns "Drink maker makes 1 chocolate with no sugar - and therefore no stick"', () => {
-    //Given
-    const input = "H::";
-    const expected = "Drink maker makes 1 chocolate with no sugar - and therefore no stick";
+  //Given
+  const input = "H::";
+  const expected =
+    "Drink maker makes 1 chocolate with no sugar - and therefore no stick";
 
-    //When
-    const result = coffeeMachine(input);
+  //When
+  const result = coffeeMachine(input);
 
-    //Then
-    expect(result).toBe(expected);
+  //Then
+  expect(result).toBe(expected);
 });
-
-
