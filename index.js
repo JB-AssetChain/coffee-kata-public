1877f529c77183e1719b173cf9ceef5525e6986d
Nick <nick@192-168-1-114.tpgi.com.au>
2021-07-15 16:58:01 +1000

    Coffee machine makes T:1:0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function coffeeMachine(a, b) {
-    return a + b;
+function coffeeMachine(input) {
+    return "Drink maker makes 1 tea with 1 sugar and a stick";
 }
 module.exports = coffeeMachine;
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
 const coffeeMachine = require('../index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(coffeeMachine(1, 2)).toBe(3);
+test('Given "T:1:0" returns "Drink maker makes 1 tea with 1 sugar and a stick"', () => {
+    //Given
+    const input = "T:1:0";
+    const expected = "Drink maker makes 1 tea with 1 sugar and a stick";
+
+    //When
+    const result = coffeeMachine(input);
+
+    //Then
+    expect(result).toBe(expected);
 });
