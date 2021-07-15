5c06a300e0ba12c335fdfd83f4265434ff58fc7b
Taiga Matsumoto <taiga@getfu.com.au>
2021-07-15 17:02:28 +1000

    make second qustion pass

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function coffeeMachine(input) {
-    return "Drink maker makes 1 tea with 1 sugar and a stick";
+    if(input === 'T:1:0'){
+        return "Drink maker makes 1 tea with 1 sugar and a stick";
+    }
+    return "Drink maker makes 1 chocolate with no sugar - and therefore no stick"
 }
 module.exports = coffeeMachine;
--- test/index.test.js
+++ test/index.test.js
@@ -11,3 +11,16 @@ test('Given "T:1:0" returns "Drink maker makes 1 tea with 1 sugar and a stick"',
     //Then
     expect(result).toBe(expected);
 });
+test('Given "H::" returns "Drink maker makes 1 chocolate with no sugar - and therefore no stick"', () => {
+    //Given
+    const input = "H::";
+    const expected = "Drink maker makes 1 chocolate with no sugar - and therefore no stick";
+
+    //When
+    const result = coffeeMachine(input);
+
+    //Then
+    expect(result).toBe(expected);
+});
+
+
