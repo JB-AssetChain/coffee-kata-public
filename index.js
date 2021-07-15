39b9763dc569f5cf784dc6cfb94dcec718bf8b9e
Tim Gaul <tim@getfu.com.au>
2021-07-15 17:23:46 +1000

    Coffee machine makes C:2:0

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function coffeeMachine(input) {
-    if(input === 'T:1:0'){
-        return "Drink maker makes 1 tea with 1 sugar and a stick";
-    }
-    return "Drink maker makes 1 chocolate with no sugar - and therefore no stick"
+  const array = input.split(":");
+  return `Drink maker makes 1 ${getDrink(array[0])} with ${getSugar(
+    array[1]
+  )} ${getStick(array[2])}`;
 }
+
+const getDrink = (input) => {
+  if (input === "C") return "coffee";
+  else if (input === "T") return "tea";
+  return "chocolate";
+};
+
+const getSugar = (input) => {
+  if (Number(input) === 1) {
+    return "1 sugar";
+  } else if (Number(input) > 1) {
+    return `${input} sugars`;
+  }
+  return "no sugar";
+};
+
+const getStick = (input) => {
+  return input ? "and a stick" : "- and therefore no stick";
+};
+
 module.exports = coffeeMachine;
--- test/index.test.js
+++ test/index.test.js
@@ -11,12 +11,24 @@ test('Given "T:1:0" returns "Drink maker makes 1 tea with 1 sugar and a stick"',
   //Then
   expect(result).toBe(expected);
 });
-/// comment
+
 test('Given "H::" returns "Drink maker makes 1 chocolate with no sugar - and therefore no stick"', () => {
   //Given
   const input = "H::";
   const expected =
     "Drink maker makes 1 chocolate with no sugar - and therefore no stick";
+
+  //When
+  const result = coffeeMachine(input);
+
+  //Then
+  expect(result).toBe(expected);
+});
+
+test('Given "C:2:0" returns "Drink maker makes 1 coffee with 2 sugars and a stick"', () => {
+  //Given
+  const input = "C:2:0";
+  const expected = "Drink maker makes 1 coffee with 2 sugars and a stick";
 
   //When
   const result = coffeeMachine(input);
